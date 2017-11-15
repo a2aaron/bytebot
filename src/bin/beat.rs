dba4f8e7b53f29c4fe305921de4185e4f663961c
Aaron Kofsky <a2aarontothe2@gmail.com>
2017-11-14 21:38:12 -0500

    Experiment: Use totally random bytebeat generation

--- src/bin/beat.rs
+++ src/bin/beat.rs
@@ -2,13 +2,17 @@ extern crate bytebeat;
 use std::io::{Read, Write};
 
 fn main() {
-    let mut code = String::new();
-    std::io::stdin().read_to_string(&mut code).unwrap();
-    let code = bytebeat::parse_beat(&code).unwrap();
-    for i in 0..60 {
-        let buf: Vec<_> = (i * 8000..(i + 1) * 8000)
-            .map(|t| bytebeat::eval_beat(&code, t as f64).unwrap() as u8)
-            .collect();
-        std::io::stdout().write_all(&buf[..]).unwrap();
+    // let mut code = String::new();
+    // let code = "t -1 <<".to_string();
+    // std::io::stdin().read_to_string(&mut code).unwrap();
+    // let code = bytebeat::parse_beat(&code).unwrap();
+    for j in 0..20 {
+        let code = bytebeat::random_beat();
+        for i in 0..5 {
+            let buf: Vec<_> = (i * 8000..(i + 1) * 8000)
+                .map(|t| bytebeat::eval_beat(&code, t as f64).unwrap() as u8)
+                .collect();
+            std::io::stdout().write_all(&buf[..]).unwrap();
+        }
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,8 @@
-#[derive(Debug, PartialEq)]
+extern crate rand;
+
+use rand::Rng;
+
+#[derive(Clone, Copy, Debug, PartialEq)]
 pub enum Cmd {
     Var,
     Num(f64),
@@ -22,6 +26,44 @@ pub enum Cmd {
     DivF,
     ModF,
 }
+
+use Cmd::*;
+static OPERATORS: [Cmd; 15] = [Var, Var, Var, Var, Num(-1.0), Num(-1.0), Sub, Mul, Div, Mod, Shl, Shr, And, Orr, Xor];
+static OPERATORS2: [Cmd; 9] = [Sub, Mul, Div, Mod, Shl, Shr, And, Orr, Xor];
+static VALUES: [Cmd; 1] = [Var];
+pub fn random_beat() -> Vec<Cmd> {
+    let mut vec = Vec::new();
+    let mut num_args = 0;
+    while vec.len() < 10 || num_args > 1 {
+        let mut random_cmd = *match num_args {
+            0 | 1 => {
+                rand::thread_rng().choose(&VALUES).unwrap()
+            },
+            _ => {
+                if vec.len() > 10 {
+                    rand::thread_rng().choose(&OPERATORS2).unwrap()
+                } else {
+                    rand::thread_rng().choose(&OPERATORS).unwrap()
+                }
+            },
+        };
+
+        num_args += match random_cmd {
+            Var | Num(_) => 1,
+            _ => -1,
+        };
+
+        if let Num(x) = random_cmd {
+            let random_number = (rand::thread_rng().gen::<i32>() % 256).abs();
+            random_cmd = Num(random_number as f64);
+        }
+
+        vec.push(random_cmd);   
+    }
+    eprintln!("{}", format_beat(&vec));
+    vec
+}
+
 
 pub fn eval_beat(cmds: &[Cmd], t: f64) -> Result<f64, ()> {
     use Cmd::*;
