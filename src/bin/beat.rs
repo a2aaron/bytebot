57c40f4c24904867e8db36957f25475dfc6e490c
Aaron Kofsky <a2aarontothe2@gmail.com>
2017-11-14 22:32:00 -0500

    Add goal length for random bytebeats.

--- src/bin/beat.rs
+++ src/bin/beat.rs
@@ -1,13 +1,16 @@
 extern crate bytebeat;
+extern crate rand;
 use std::io::{Read, Write};
+use rand::Rng;
 
 fn main() {
     // let mut code = String::new();
     // let code = "t -1 <<".to_string();
     // std::io::stdin().read_to_string(&mut code).unwrap();
     // let code = bytebeat::parse_beat(&code).unwrap();
-    for j in 0..20000 {
-        let code = bytebeat::random_beat();
+    for j in 0..200 {
+        let length = rand::thread_rng().gen_range::<usize>(1, 16);
+        let code = bytebeat::random_beat(length);
         for i in 0..1 {
             let buf: Vec<_> = (i * 8000..(i + 1) * 8000)
                 .map(|t| bytebeat::eval_beat(&code, t as f64).unwrap() as u8)
--- src/lib.rs
+++ src/lib.rs
@@ -31,16 +31,16 @@ use Cmd::*;
 static OPERATORS: [Cmd; 15] = [Var, Var, Var, Var, Num(-1.0), Num(-1.0), Sub, Mul, Div, Mod, Shl, Shr, And, Orr, Xor];
 static OPERATORS2: [Cmd; 9] = [Sub, Mul, Div, Mod, Shl, Shr, And, Orr, Xor];
 static VALUES: [Cmd; 1] = [Var];
-pub fn random_beat() -> Vec<Cmd> {
+pub fn random_beat(goal_length: usize) -> Vec<Cmd> {
     let mut vec = Vec::new();
     let mut num_args = 0;
-    while vec.len() < 10 || num_args > 1 {
+    while vec.len() < goal_length || num_args > 1 {
         let mut random_cmd = *match num_args {
             0 | 1 => {
                 rand::thread_rng().choose(&VALUES).unwrap()
             },
             _ => {
-                if vec.len() > 10 {
+                if vec.len() > goal_length {
                     rand::thread_rng().choose(&OPERATORS2).unwrap()
                 } else {
                     rand::thread_rng().choose(&OPERATORS).unwrap()
