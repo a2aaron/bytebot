ecbba87ca0005330ff54cf27f3b25fcf7c34c358
Aaron Kofsky <a2aarontothe2@gmail.com>
2017-11-15 23:16:46 -0500

    Use one liner for determining if a bytebeat's output is silent.
    This is much easier to read and is more idomatic.
    
    Also clean up one unused variable.

--- src/bin/random.rs
+++ src/bin/random.rs
@@ -6,22 +6,18 @@ use rand::Rng;
 
 
 fn main() {
-    for j in 0..200 {
+    for _ in 0..200 {
         let length = rand::thread_rng().gen_range::<usize>(1, 16);
         let code = bytebeat::random::random_beat(length);
         let buf: Vec<_> = (0..8000)
             .map(|t| bytebeat::eval_beat(&code, t as f64).unwrap() as u8)
             .collect();
 
-        let mut is_silent = true;
-        for ele in &buf {
-            if *ele != 0 {
-                is_silent = false;
-                break;
-            }
-        }
+        let is_silent = buf.iter().all(|&x| x == buf[0]);
 
         if is_silent {
+            // Write to standard error because writing to stdout will 
+            // pipe that to sox/mplayer.
             eprintln!("Skipping: {}", bytebeat::format_beat(&code))
         } else {
             std::io::stdout().write_all(&buf[..]).unwrap();
