bb2cd754938142d4e1ad0a69d6f26a2fbd2e261c
Aaron Kofsky <a2aarontothe2@gmail.com>
2017-11-15 23:34:30 -0500

    Generate random Num and Var together.

--- src/random.rs
+++ src/random.rs
@@ -3,12 +3,12 @@ use super::Cmd;
 use Cmd::*;
 
 static OPERATORS: [Cmd; 15] = [
+    Var, // Var is used as a stand in for Var or Num
     Var,
     Var,
     Var,
     Var,
-    Num(-1.0),
-    Num(-1.0),
+    Var,
     Sub,
     Mul,
     Div,
@@ -20,34 +20,38 @@ static OPERATORS: [Cmd; 15] = [
     Xor,
 ];
 static OPERATORS2: [Cmd; 9] = [Sub, Mul, Div, Mod, Shl, Shr, And, Orr, Xor];
-static VALUES: [Cmd; 1] = [Var];
 
 pub fn random_beat(goal_length: usize) -> Vec<Cmd> {
     let mut vec = Vec::new();
     let mut num_args = 0;
     while vec.len() < goal_length || num_args > 1 {
-        let mut random_cmd = *match num_args {
-            0 | 1 => rand::thread_rng().choose(&VALUES).unwrap(),
+        let mut random_cmd = match num_args {
+            0 | 1 => Var,
             _ => {
                 if vec.len() > goal_length {
-                    rand::thread_rng().choose(&OPERATORS2).unwrap()
+                    *rand::thread_rng().choose(&OPERATORS2).unwrap()
                 } else {
-                    rand::thread_rng().choose(&OPERATORS).unwrap()
+                    *rand::thread_rng().choose(&OPERATORS).unwrap()
                 }
             }
         };
+
+        if let Var = random_cmd {
+            random_cmd = random_value();
+        }
 
         num_args += match random_cmd {
             Var | Num(_) => 1,
             _ => -1,
         };
 
-        if let Num(_) = random_cmd {
-            let random_number = (rand::thread_rng().gen::<i32>() % 256).abs();
-            random_cmd = Num(random_number as f64);
-        }
-
         vec.push(random_cmd);
     }
     vec
+}
+
+/// Returns either a Var or a Num in the range [0, 256)
+fn random_value() -> Cmd {
+    let number = rand::thread_rng().gen_range::<i32>(0, 256);
+    *rand::thread_rng().choose(&[Cmd::Var, Cmd::Num(number as f64)]).unwrap()
 }
