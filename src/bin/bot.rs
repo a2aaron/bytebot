6dba7f6a14d365ef415b011588fe7dd26e6a7e47
Caleb Jones <code@calebjones.net>
2017-11-23 02:40:29 -0500

    Clean up bot code

--- src/bin/bot.rs
+++ src/bin/bot.rs
@@ -3,51 +3,57 @@ extern crate dotenv;
 extern crate egg_mode;
 extern crate tokio_core;
 
-use std::io::Read;
+use std::io::{self, Read};
 use std::env;
 
+use bytebeat::Cmd;
+use dotenv::dotenv;
 use egg_mode::{media, Token, KeyPair};
 use egg_mode::tweet::DraftTweet;
+use tokio_core::reactor::Core;
 
 mod util;
 
 
 fn main() {
-    dotenv::dotenv().unwrap();
+    dotenv().ok();
     let auth = get_auth().unwrap();
-    let mut core = tokio_core::reactor::Core::new().unwrap();
+    let mut core = Core::new().unwrap();
     let handle = core.handle();
 
-    let beat = "t t >> "; // 255 % & t 13 >> t & -";
-    let code = bytebeat::parse_beat(beat).unwrap();
+    println!("Generating beat...");
+    let code = generate_beat();
+    let text = bytebeat::format_beat(&code);
+    println!("Generated!");
 
-    println!("Encoding video...");
-    util::generate_video(&code, "out.mp4");
-    let data = {
-        let mut data = Vec::new();
-        std::fs::File::open("out.mp4")
-            .unwrap()
-            .read_to_end(&mut data)
-            .unwrap();
-        data
-    };
-    println!("Encoded!");
+    println!("Rendering video...");
+    let video_data = encode_video(&code).unwrap();
+    println!("Rendered!");
 
     println!("Uploading video to twitter...");
-    let media_handle = core.run(
-        media::UploadBuilder::new(&data[..], media::media_types::video_mp4())
-            .category(media::MediaCategory::Video)
-            .call(&auth, &handle),
-    ).unwrap();
+    let upload = media::UploadBuilder::new(&video_data[..], media::media_types::video_mp4())
+        .category(media::MediaCategory::Video);
+    let media_handle = core.run(upload.call(&auth, &handle)).unwrap();
     println!("Uploaded!");
 
     println!("Posting tweet...");
     core.run(
-        DraftTweet::new(beat)
+        DraftTweet::new(text)
             .media_ids(&[media_handle.media_id][..])
             .send(&auth, &handle),
     ).unwrap();
     println!("Posted!");
+}
+
+fn generate_beat() -> Vec<Cmd> {
+    bytebeat::parse_beat("255 % & t 13 >> t & -").unwrap()
+}
+
+fn encode_video(code: &[Cmd]) -> io::Result<Vec<u8>> {
+    util::generate_video(&code, "out.mp4");
+    let mut data = Vec::new();
+    std::fs::File::open("out.mp4")?.read_to_end(&mut data)?;
+    Ok(data)
 }
 
 fn get_auth() -> Result<Token, std::env::VarError> {
