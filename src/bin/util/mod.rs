8f6e9c179e0d4696628a5eaa0ff1fd15624bd6fb
Caleb Jones <code@calebjones.net>
2017-11-25 21:25:41 -0500

    Move the waveform below the background instead of overlaid

--- src/bin/util/mod.rs
+++ src/bin/util/mod.rs
@@ -3,15 +3,17 @@ extern crate bytebeat;
 use bytebeat::Program;
 use bytebeat::encode::{EncoderConfig, Color};
 
-const WIDTH: usize = 512;
-const HEIGHT: usize = 256;
+const WIDTH: usize = 640;
+const HEIGHT: usize = 360;
+const BG_HEIGHT: usize = 256;
+const WV_HEIGHT: usize = HEIGHT - BG_HEIGHT;
 const FPS: usize = 15;
 
 pub fn generate_video(code: &Program, fname: &str) {
     let hz = code.hz().unwrap_or(8000) as usize;
     let frame_count = FPS * 30;
     let size = frame_count * hz / FPS;
-    let full_width = size / HEIGHT;
+    let full_width = size / BG_HEIGHT;
 
     // Generate the audio data
     let data = {
@@ -66,32 +68,43 @@ fn render_frame(
     scan_color: Color,
 ) {
     // Draw the background
-    for i in 0..WIDTH * HEIGHT {
-        let idx = (i % HEIGHT) * WIDTH + (i / HEIGHT);
-        let x = data[i + bg_offset * HEIGHT];
-        image[idx] = bg_color * x;
+    for row in 0..BG_HEIGHT {
+        for col in 0..WIDTH {
+            let i = row + col * BG_HEIGHT;
+            let x = data[i + bg_offset * BG_HEIGHT];
+            image[row * WIDTH + col] = bg_color * x;
+        }
+    }
+
+    // Clear the waveform
+    for row in BG_HEIGHT..HEIGHT {
+        for col in 0..WIDTH {
+            image[row * WIDTH + col] = Color([0, 0, 0]);
+        }
     }
 
     // Draw the waveform
     let waveform_col = bg_offset + cursor_col;
-    for row in 0..HEIGHT - 1 {
-        let x0 = data[waveform_col * HEIGHT + row] as usize;
-        let x1 = data[waveform_col * HEIGHT + row + 1] as usize;
+    for sample in 0..BG_HEIGHT - 1 {
+        let x0 = data[waveform_col * BG_HEIGHT + sample] as usize * WV_HEIGHT / 256;
+        let x1 = data[waveform_col * BG_HEIGHT + sample + 1] as usize * WV_HEIGHT / 256;
         let mid = ((x0 + x1) / 2) as usize;
-        let col = row * WIDTH / HEIGHT;
+        let col = sample * WIDTH / BG_HEIGHT;
 
         for r in x0.min(mid)..x0.max(mid) + 1 {
-            image[(255 - r) * WIDTH + col] = wave_color;
-            image[(255 - r) * WIDTH + col + 1] = wave_color;
+            let row = HEIGHT - r - 1;
+            image[row * WIDTH + col] = wave_color;
+            image[row * WIDTH + col + 1] = wave_color;
         }
         for r in x1.min(mid)..x1.max(mid) + 1 {
-            image[(255 - r) * WIDTH + col + 1] = wave_color;
-            image[(255 - r) * WIDTH + col + 2] = wave_color;
+            let row = HEIGHT - r - 1;
+            image[row * WIDTH + col + 1] = wave_color;
+            image[row * WIDTH + col + 2] = wave_color;
         }
     }
 
     // Draw the cursor
-    for row in 0..HEIGHT {
+    for row in 0..BG_HEIGHT {
         if cursor_col > 0 {
             image[row * WIDTH + cursor_col] = scan_color;
         }
