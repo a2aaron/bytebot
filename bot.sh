07423ff2f2288cd1fc943d59cd8d8ed071b4d14b
C Jones <code@calebjones.net>
2017-11-26 00:23:29 -0500

    Make the bot properly format two-line tweets

--- bot.sh
+++ bot.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 date >> ~/logs/bytebot.log
-head -1 .post-queue | ./target/release/bot >> ~/logs/bytebot.log 2>&1
-sed -ie 1d .post-queue
+head -2 .post-queue | timeout 300 ./target/release/bot >> ~/logs/bytebot.log 2>&1
+sed -ie '1d;2d' .post-queue
