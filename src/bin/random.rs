18e2ceb5706a9d2d7d0e5aadc7b2e89bd11847ee
Aaron Kofsky <a2aarontothe2@gmail.com>
2017-12-02 16:41:27 -0500

    Make bin/random.rs output a single formula
    This allows it to be chained with bot.rs and encode.rs

--- src/bin/random.rs
+++ src/bin/random.rs
@@ -6,30 +6,27 @@ use bytebeat::random;
 mod util;
 
 fn main() {
-    for _ in 0..10 {
-        let length = rand::thread_rng().gen_range::<usize>(2, 7);
+    let code = gen_beat(2, 7);
+    println!("{}", code);
+}
+
+fn gen_beat(min: usize, max: usize) -> bytebeat::Program {
+    loop {
+        let length = rand::thread_rng().gen_range::<usize>(min, max);
         let code = random::random_t_multiply(length);
-        println!("{}", bytebeat::format_beat(&code));
-    }
+        let code = bytebeat::compile(code).unwrap();
+        let buf: Vec<_> = (0..5 * 8000)
+            .map(|t| bytebeat::eval_beat(&code, t))
+            .collect();
 
-    let length = rand::thread_rng().gen_range::<usize>(2, 7);
-    let code = random::random_t_multiply(length);
-    println!("{}", bytebeat::format_beat(&code));
-    let code = bytebeat::compile(code).unwrap();
-    // eprintln!("{}", bytebeat::format_beat(&code));
-    let buf: Vec<_> = (0..5 * 8000)
-        .map(|t| bytebeat::eval_beat(&code, t))
-        .collect();
+        let is_silent = buf.iter().all(|&x| x == buf[0]);
 
-    let is_silent = buf.iter().all(|&x| x == buf[0]);
-
-    if is_silent {
-        // Write to standard error because writing to stdout will
-        // pipe that to sox/mplayer.
-        eprintln!("Skipping: {}", code)
-    } else {
-        println!("Encoding... (formula: {})", code);
-        util::generate_video(&code, "out.mp4");
-        eprintln!("{}", code);
+        if is_silent {
+            // Write to standard error because writing to stdout will
+            // pipe that to sox/mplayer.
+            eprintln!("Skipping: {}", code)
+        } else {
+            return code;
+        }
     }
 }
