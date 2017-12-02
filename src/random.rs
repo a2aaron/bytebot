54462463908dea5d71a32711efe3f2ebb3c0c441
Aaron Kofsky <a2aarontothe2@gmail.com>
2017-12-02 16:55:36 -0500

    Add the grammar to the comments for the random generators

--- src/random.rs
+++ src/random.rs
@@ -17,14 +17,22 @@ use Cmd::*;
 /// composed with a bitwise operator. For example,
 /// `t * ((t>>8) ^ (t>>3) | (t>>2))` is a t multiply bytebeat
 /// (In RPN: `t t 8 >> t 3 >> ^ t 2 >> | *`)
+/// Grammar:
+/// t_multiple := t_like, oscillator, Mul
+/// Where t_like and oscillator are defined below
 pub fn random_t_multiply(goal_length: usize) -> Vec<Cmd> {
     let t_like = random_t_like(goal_length / 2);
     let oscillator = random_oscillator(goal_length / 2);
     compose(t_like, oscillator, Mul)
 }
 
-/// Returns a "t like" formula with `length` amount of
-/// bytebeat units
+/// Returns a "t like" bytebeat unit with `length` amount of
+/// bytebeat units. Length is recommended to be 3 or less.
+/// Grammar is below.
+/// t_like := unit, {op, unit}
+/// unit := Var, num, Shr
+/// op := AnyTwoArgOp
+/// num := 0...3
 fn random_t_like(length: usize) -> Vec<Cmd> {
     let mut vec = random_t_shr(0, 13);
     for _ in 0..length {
@@ -34,6 +42,13 @@ fn random_t_like(length: usize) -> Vec<Cmd> {
     vec
 }
 
+/// Returns a "oscillator" bytebeat unit with `length` amount of
+/// bytebeat units. Length is recommended to be 5 or below.
+/// Grammar is below
+/// oscillator := unit, {op, unit}
+/// unit := Var, num, Shr
+/// op := Mul | Mod | And | Orr | Xor
+/// num := 0...13
 fn random_oscillator(length: usize) -> Vec<Cmd> {
     let mut vec = random_t_shr(0, 13);
     for _ in 0..length {
@@ -58,6 +73,7 @@ fn random_t_shr(min: i64, max: i64) -> Vec<Cmd> {
     }
 }
 
+/// Given bytebeat units `left` and `right`, returns `left right op`
 fn compose(mut left: Vec<Cmd>, right: Vec<Cmd>, two_arg_op: Cmd) -> Vec<Cmd> {
     left.extend(right);
     left.push(two_arg_op);
