32b86626376a191c13f100945aae8e534e72ec81
Aaron Kofsky <a2aarontothe2@gmail.com>
2017-12-03 13:52:31 -0500

    Make fuzzer also choose a time other than zero.
    The first value in the u8 stream is now the time for the bytebeat to be evaluated at.

--- fuzz/fuzz_targets/evaluator.rs
+++ fuzz/fuzz_targets/evaluator.rs
@@ -3,10 +3,13 @@
 extern crate bytebeat;
 
 fuzz_target!(|data: &[u8]| {
-    if let Ok(s) = std::str::from_utf8(data) {
-        if let Ok(program) = bytebeat::parse_beat(s) {
-            if let Ok(program) = bytebeat::compile(program) {
-                let _ = bytebeat::eval_beat(&program, 0.0);
+    if data.len() >= 2 {
+        let time = data[0] as i64;
+        if let Ok(s) = std::str::from_utf8(&data[1..]) {
+            if let Ok(program) = bytebeat::parse_beat(s) {
+                if let Ok(program) = bytebeat::compile(program) {
+                    let _ = bytebeat::eval_beat(&program, time);
+                }
             }
         }
     }
