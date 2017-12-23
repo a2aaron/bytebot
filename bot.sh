bbaf064e19c8e7cab99a23d210b3b268f7a7e5d2
Aaron Kofsky <a2aarontothe2@gmail.com>
2017-12-22 19:09:15 -0500

    Make bot.sh do random generation once in a while

--- bot.sh
+++ bot.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
 date >> ~/logs/bytebot.log
-head -2 .post-queue | timeout 300 ./target/release/bot >> ~/logs/bytebot.log 2>&1
-sed -ie '1d;2d' .post-queue
+number=$RANDOM
+let "number %= 4"
+if [ "$number" -eq 0 ]
+then
+    echo "random"
+    ./target/release/random | tee ~/logs/bytebot.log 2>&1 | ./target/release/bot
+else
+    echo "curated"
+    head -2 .post-queue | timeout 300 ./target/release/bot >> ~/logs/bytebot.log 2>&1
+    sed -ie '1d;2d' .post-queue
+fi
