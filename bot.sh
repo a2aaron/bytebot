37dc6434bfa9c44851c444e89867af6e327a0868
Aaron Kofsky <a2aarontothe2@gmail.com>
2018-01-13 22:59:49 -0500

    Disable "bbcurated" generation

--- bot.sh
+++ bot.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
 date >> ~/logs/bytebot.log
-number=$RANDOM
-let "number %= 4"
-if [ "$number" -eq 0 ]
-then
-    echo "random"
-    ./target/release/random | python3 gencol.py '#bbrandom' | timeout 300 ./target/release/bot >> ~/logs/bytebot.log 2>&1
-else
-    echo "curated"
-    head -2 .post-queue | timeout 300 ./target/release/bot >> ~/logs/bytebot.log 2>&1
-    sed -ie '1d;2d' .post-queue
-fi
+./target/release/random | python3 gencol.py '#bbrandom' | timeout 300 ./target/release/bot >> ~/logs/bytebot.log 2>&1
+
+# number=$RANDOM
+# let "number %= 4"
+# if [ "$number" -eq 0 ]
+# then
+#     echo "random"
+#     ./target/release/random | python3 gencol.py '#bbrandom' | timeout 300 ./target/release/bot >> ~/logs/bytebot.log 2>&1
+# else
+#     echo "curated"
+#     head -2 .post-queue | timeout 300 ./target/release/bot >> ~/logs/bytebot.log 2>&1
+#     sed -ie '1d;2d' .post-queue
+# fi
